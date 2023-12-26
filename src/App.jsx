9e07b197dded85afad737e62c2041c2d14569833
datdudu <110999440+datdudu@users.noreply.github.com>
2023-12-26 22:34:29 +0000

    Udating portifolio with my personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Eduardo Carvalho",
+  title: "Fullstack Developer & Technology Enthusiast",
+  email: "c.eduardocarvalhoc@gmail.com",
+  gitHub: "datdudu",
+  instagram: "duducsz",
+  linkedIn: "c-eduardocarvalho",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "datduduc",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#0d014d";
+const secondaryColor = "#94a6ff";
 
 const App = () => {
   return (
